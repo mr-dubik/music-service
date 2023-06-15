6f9ebc2483ba91de1e82c038c0a64e1fa44df06d
Valentin <119943910+mr-dubik@users.noreply.github.com>
2023-06-15 09:13:42 +0300

    onChange input
    add onChange input

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,33 +1,43 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom'
 import Logo from '../../img/logo_black.png'
 
 import './style.css'
 
 function LoginPage() {
+  const [inputLogin, setInputLogin] = useState('');
+  const [inputPassword, setInputPassword] = useState('');
+
+  const clg = () => {
+    console.log(inputLogin)
+    console.log(inputPassword)
+  }
+
   return (
     <div className="wrapper">
-      <div className="wrapper__login">
+      <form className="wrapper__login">
         <img src={Logo} alt="Logo" />
         <div className="login__inputs">
-          <input type="text" className="input__login" placeholder="Логин" />
+          <input type="text" className="input__login" placeholder="Логин" onChange={(event) => setInputLogin(event.target.value)}/>
           <div className="border" />
           <input
             type="password"
             className="input__password"
             placeholder="Пароль"
+            onChange={(event) => setInputPassword(event.target.value)}
           />
           <div className="border" />
         </div>
 
         <div className="login__buttons">
-          <NavLink type="button" className="login__button" to='/home'>
+          <NavLink type="button" className="login__button" to='/home' onClick={clg}>
             Войти
           </NavLink>
           <NavLink type="button" className="register__button" to='/register'>
             Зарегестироваться
           </NavLink>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
